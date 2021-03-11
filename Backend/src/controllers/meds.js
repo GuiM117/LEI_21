a80b8a645ef81bdf3d3c97ff1439c4cab5e3b1ab
GuiM117 <36679610+GuiM117@users.noreply.github.com>
2021-03-11 23:43:59 +0000

    Models and controllers for meds (#7)
    Routes not comiited yet :construction:
    (close #4)

---
+++ Backend/src/controllers/meds.js
@@ -0,0 +1,25 @@
+const Med = require ('../models/meds');
+
+//Devolve a lista de meds
+module.exports.listar = () => {
+    return Med
+        .find()
+        .exec()
+}
+//Consulta um med especifico por id
+module.exports.consultar = id => {
+    return Med
+        .findOne({_id: id})
+        .exec()
+}
+//Insere um med
+module.exports.inserir = m => {
+    let newMed = new Med(m)
+    return newMed.save()
+}
+
+//Remove um med
+module.exports.remover = id => {
+    return Med
+        .deleteOne({_id: id})
+}
