33bbfe77890326567991a290e0aa5b28128e72ee
Sarah250 <Sarah250@github.com>
2021-04-01 17:53:02 +0100

    minor stuff

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import '../styles/NavBar.css'
 
+
+
 const navBar = () => {
     return (
-        <div className="navBar"/>
+        <div className="navBar">
+            <button onClick="activateLasers()" className="button">
+                Hello
+            </button>
+        </div>
     )
 }
 
