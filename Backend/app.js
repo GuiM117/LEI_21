60310db36bfc9813ae34b5a2c3923990f59a3c7b
Sarah250 <Sarah250@github.com>
2021-04-13 20:13:36 +0100

    Data Migration Bug fixed :bug:

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,10 @@
-
+const povoamento = require('./src/utilities/dataset')
 require('dotenv').config()
 
 const app = require('./server/server')
 
 
-app.listen(process.env.APP_PORT, () => console.log(`Server listening on port ${process.env.APP_PORT}`))
+app.listen(process.env.APP_PORT, () => {
+    povoamento
+    console.log(`Server listening on port ${process.env.APP_PORT}`)
+})
