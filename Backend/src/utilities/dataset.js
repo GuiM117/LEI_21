71f690ae52415865633da67857e7b2c0c0197c25
Sarah250 <Sarah250@github.com>
2021-05-20 15:28:02 +0100

    More updates

--- Backend/src/utilities/dataset.js
+++ Backend/src/utilities/dataset.js
@@ -21,58 +21,58 @@ var interacoes = [];
 
 const readerCaracteizacao =
     fs.createReadStream(dataPathCaracterizacao)
-    .pipe(csv())
+    .pipe(csv({separator:";"}))
     .on('data', async(row) => {
-        //console.log(JSON.stringify(row));
+
         let entries = Object.values(row);
-        let fields = entries[0].split(";");
         let adminForm_IDS = []
 
         for (let i = 11; i<21; i++ ) {
-            if (fields[i]) adminForm_IDS.push(fields[i])
+            if (entries[i]) adminForm_IDS.push(entries[i])
         }
 
 
         let Med = {
-            chnm: fields[0],
-            name: fields[3],
-            dci_ID: fields[4],
-            farmForm_ID: fields[6],
-            containerForm_ID: fields[5],
-            capacity: fields[9],
-            capacity_unit: fields[10],
+            chnm: entries[0],
+            name: entries[3],
+            dci_ID: entries[4],
+            farmForm_ID: entries[6],
+            containerForm_ID: entries[5],
+            capacity: entries[9],
+            capacity_unit: entries[10],
             administrationForm_IDs: adminForm_IDS
         }
         meds.push(Med)
     })
     .on('end', () => {
-        //console.log('CSV file successfully processed');
+        console.log('TabMestre_DCI file successfully processed');
         MedV.inserirMts(meds)
-            .then(() => console.log('CSV file successfully processed'))
+            .then(() => console.log('[Caracterização] Data Migration Concluded'))
             .catch(e => console.log(e))
     });
 
 module.exports = readerCaracteizacao
 
+
 const readerTabMestreDCI =
     fs.createReadStream(dataPathTabMestre)
-        .pipe(csv())
+        .pipe(csv({separator:";"}))
         .on('data', async(row) => {
             //console.log(JSON.stringify(row));
             var entries = Object.values(row);
-            var fields = entries[0].split(";");
-            if (fields[0]=="DCI") {
+
+            if (entries[0]=="DCI") {
                 let DCI = {
-                    _id: fields[1],
-                    description: fields[2]
+                    _id: entries[1],
+                    description: entries[2]
                 }
                 mestreDCI.push(DCI)
             }
         })
         .on('end', () => {
-            console.log('CSV file successfully processed');
+            console.log('TabMestre_DCI file successfully processed');
             MestreDCIV.inserirMts(mestreDCI)
-                .then(dados => console.log("Data Migration Concluded"))
+                .then(dados => console.log("[TabMestre_DCI] Data Migration Concluded"))
                 .catch(e => console.log(e))
         });
 
@@ -80,24 +80,24 @@ module.exports = readerTabMestreDCI
 
 const readerTabMestrePharmForm =
     fs.createReadStream(dataPathTabMestre)
-        .pipe(csv())
+        .pipe(csv({separator:";"}))
         .on('data', async(row) => {
             //console.log(JSON.stringify(row));
             var entries = Object.values(row);
-            var fields = entries[0].split(";");
-            if (fields[0]=="FORMA_FARM"){
+
+            if (entries[0]=="FORMA_FARM"){
                 let PharmForm = {
-                    _id: fields[1],
-                    description: fields[2]
+                    _id: entries[1],
+                    description: entries[2]
                 }
                 mestrePharmForm.push(PharmForm)
             }
 
         })
         .on('end', () => {
-            console.log('CSV file successfully processed');
+            console.log('TabMestre_PharmaForm file successfully processed');
             MestrePharmFormV.inserirMts(mestrePharmForm)
-                .then(dados => console.log("Data Migration Concluded"))
+                .then(dados => console.log("[TabMestre_PharmaForm] Data Migration Concluded"))
                 .catch(e => console.log(e))
         });
 
@@ -105,24 +105,23 @@ module.exports = readerTabMestrePharmForm
 
 const readerTabMestreUnitMed =
     fs.createReadStream(dataPathTabMestre)
-        .pipe(csv())
+        .pipe(csv({separator:";"}))
         .on('data', async(row) => {
-            //console.log(JSON.stringify(row));
             var entries = Object.values(row);
-            var fields = entries[0].split(";");
-            if (fields[0]=="UNID_MED"){
+
+            if (entries[0]=="UNID_MED"){
                 let unitMed = {
-                    _id: fields[1],
-                    description: fields[2]
+                    _id: entries[1],
+                    description: entries[2]
                 }
                 mestreUnitMed.push(unitMed)
             }
 
         })
         .on('end', () => {
-            console.log('CSV file successfully processed');
+            console.log('TabMestre_UnitMed file successfully processed');
             MestreUnitMedV.inserirMts(mestreUnitMed)
-                .then(dados => console.log("Data Migration Concluded"))
+                .then(dados => console.log("[TabMestre_UnitMed] Data Migration Concluded"))
                 .catch(e => console.log(e))
         });
 
@@ -130,24 +129,23 @@ module.exports = readerTabMestreUnitMed
 
 const readerTabMestreViaAdmin =
     fs.createReadStream(dataPathTabMestre)
-        .pipe(csv())
+        .pipe(csv({separator:";"}))
         .on('data', async(row) => {
-            //console.log(JSON.stringify(row));
             var entries = Object.values(row);
-            var fields = entries[0].split(";");
-            if (fields[0]=="VIA_ADM"){
+
+            if (entries[0]=="VIA_ADM"){
                 let viaAdm = {
-                    _id: fields[1],
-                    description: fields[2]
+                    _id: entries[1],
+                    description: entries[2]
                 }
                 mestreViaAdmin.push(viaAdm)
             }
 
         })
         .on('end', () => {
-            console.log('CSV file successfully processed');
+            console.log('TabMestre_ViaAdmin file successfully processed');
             MestreViaAdminV.inserirMts(mestreViaAdmin)
-                .then(dados => console.log("Data Migration Concluded"))
+                .then(dados => console.log("[TabMestre_ViaAdmin] Data Migration Concluded"))
                 .catch(e => console.log(e))
         });
 
@@ -155,22 +153,22 @@ module.exports = readerTabMestreViaAdmin
 
 const readerInteracoes =
     fs.createReadStream(dataPathInteracoes)
-        .pipe(csv())
+        .pipe(csv({separator:";"}))
         .on('data', async(row) => {
             //console.log(JSON.stringify(row));
             var entries = Object.values(row);
-            var fields = entries[0].split(";");
+
             let inter = {
-                chnm1: fields[1],
-                chnm2: fields[2],
-                descriptionFull: fields[6]
+                chnm1: entries[1],
+                chnm2: entries[2],
+                descriptionFull: entries[6]
             }
             interacoes.push(inter)
         })
         .on('end', () => {
-            console.log('CSV file successfully processed');
+            console.log('Interações file successfully processed');
             InteracoesV.inserirMts(interacoes)
-                .then(dados => console.log("Interações Migration Concluded"))
+                .then(dados => console.log("[Interações] Data Migration Concluded"))
                 .catch(e => console.log(e))
         });
 
